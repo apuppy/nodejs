a4c44ec0dd33c9f113283430780fe3d9e6dda1ee
computer in office <chinalihongde@163.com>
2015-10-16 15:22:57 +0800

    a simple http request

---
+++ httpServer.js
@@ -0,0 +1,10 @@
+var http=require("http");
+var server=new http.Server();
+server.on("request",function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("Welcome to nodejs http module!");
+    res.end("It works!");
+});
+
+server.listen(3000);
+console.log("Http server is listening on port 3000……");
